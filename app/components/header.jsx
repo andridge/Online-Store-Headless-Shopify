82cb4b0d17a9742210d2bea4a9f3d343cc200896
Andridge Munene <munenengarw@gmail.com>
2023-07-06 21:16:05 +0300

    whothemaafakahatingonmehuh

---
+++ app/components/header.jsx
@@ -0,0 +1,20 @@
+import {useState } from 'react'
+export default function Footer() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    return (
+        <header className="bg-white bg-opacity-10">
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-5 lg:px-8" aria-label="Global">
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+            <span className="text-4xl font-bold text-indigo-600">
+                OPENSEASONS
+            </span>
+              <img className="h-8 w-auto" src="" alt="" />
+            </a>
+          </div>
+        </nav>
+      </header>
+    );
+
+}
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,8 +1,14 @@
 import Hero from "../components/hero";
+import Footer from "../components/footer";
+import Header from "../components/header";
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+           <Header/>
+
            <Hero />      
+
+           <Footer/>
     </div>
   );
 }
