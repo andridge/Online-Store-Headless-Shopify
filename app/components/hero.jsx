cf6d947e7b68ec6d8f95a16f41bd308a4d5ecd23
Andridge Munene <munenengarw@gmail.com>
2023-07-06 21:16:38 +0300

    whothemaafakahatingonmehuh

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -16,7 +16,7 @@ export default function Hero() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our most exiting blog post with the latest projects  in OPENSEASONS.{' '}
+              Announcing our most exciting blog post with the latest projects  in OPENSEASONS.{' '}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
